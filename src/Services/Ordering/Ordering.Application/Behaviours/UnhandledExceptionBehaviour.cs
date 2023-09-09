85f83f62292186920e40a5c9b964503b2397792b
a.momeni <alirezammn@yahoo.com>
2023-09-09 15:20:56 +0330

    MediatR Update to 11.1.0

--- src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Ordering.Application.Behaviours
 {
     public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
     {
         private readonly ILogger<TRequest> _logger;
 
@@ -15,7 +16,8 @@ namespace Ordering.Application.Behaviours
             _logger = logger;
         }
 
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
         {
             try
             {
@@ -24,7 +26,8 @@ namespace Ordering.Application.Behaviours
             catch (Exception ex)
             {
                 var requestName = typeof(TRequest).Name;
-                _logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+                _logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}",
+                    requestName, request);
                 throw;
             }
         }
--- src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -18,18 +18,24 @@ namespace Ordering.Application.Behaviours
             _validators = validators;
         }
 
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
         {
             if (_validators.Any())
             {
                 var context = new ValidationContext<TRequest>(request);
 
-                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
-                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+                var validationResults = await Task
+                    .WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+                var failures = validationResults
+                    .SelectMany(r => r.Errors)
+                    .Where(f => f != null).ToList();
 
                 if (failures.Count != 0)
                     throw new ValidationException(failures);
             }
+
             return await next();
         }
     }
