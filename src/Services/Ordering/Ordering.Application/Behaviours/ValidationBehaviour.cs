63d35936360b3096e60048dcb758e727d9de7c87
a.momeni <alirezammn@yahoo.com>
2023-09-09 17:33:31 +0330

    Clean Code

--- src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ValidationException = Ordering.Application.Exceptions.ValidationException;
 
 namespace Ordering.Application.Behaviours
 {
@@ -21,20 +20,18 @@ namespace Ordering.Application.Behaviours
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
             CancellationToken cancellationToken)
         {
-            if (_validators.Any())
-            {
-                var context = new ValidationContext<TRequest>(request);
+            if (!_validators.Any()) return await next();
+            var context = new ValidationContext<TRequest>(request);
 
-                var validationResults = await Task
-                    .WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+            var validationResults = await Task
+                .WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
 
-                var failures = validationResults
-                    .SelectMany(r => r.Errors)
-                    .Where(f => f != null).ToList();
+            var failures = validationResults
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null).ToList();
 
-                if (failures.Count != 0)
-                    throw new ValidationException(failures);
-            }
+            if (failures.Count != 0)
+                throw new ValidationException(failures);
 
             return await next();
         }
